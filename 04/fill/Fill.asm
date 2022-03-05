2cf7543e6641cd6ef0def4e83049c3f85efe0117
STRRL <str_ruiling@outlook.com>
2022-03-05 15:28:43 +0800

    chore: fill
    Signed-off-by: STRRL <str_ruiling@outlook.com>

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,63 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(LOOP)
+
+@KBD
+D=M
+@SET_WHITE
+D;JEQ
+@SET_BLACK
+0;JMP
+
+(SET_BLACK)
+// color=-1
+@color
+M=-1
+@PAINT
+0;JMP
+
+(SET_WHITE)
+// color=0
+@color
+M=0
+@PAINT
+0;JMP
+
+(PAINT)
+// i=0
+@i
+M=0
+
+@SCREEN
+D=A
+@seek
+M=D
+
+(PAINTLOOP)
+// if i>=8192; exit paint loop
+@8192
+D=A
+@i
+D=M-D // D=i-8192
+@LOOP
+D;JGE
+
+// else paint pixel
+
+@color
+D=M
+@seek
+A=M
+M=D
+
+// i=i+1
+@i
+M=M+1
+@seek
+M=M+1
+
+// paint loop
+@PAINTLOOP
+0;JMP
