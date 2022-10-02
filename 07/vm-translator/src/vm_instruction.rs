7576158aceb6d6b20ed5373f9a9a4608daf4b874
STRRL <im@strrl.dev>
2022-10-02 17:42:08 +0800

    feat: stack test
    Signed-off-by: STRRL <im@strrl.dev>

--- 07/vm-translator/src/vm_instruction.rs
+++ 07/vm-translator/src/vm_instruction.rs
@@ -1,6 +1,7 @@
 use std::vec;
 
 use anyhow::anyhow;
+use uuid::Uuid;
 
 #[derive(Debug)]
 pub enum VMInstruction {
@@ -116,101 +117,116 @@ impl ToASM for ArithmeticInstruction {
             .iter()
             .map(|s| s.to_string())
             .collect(),
-            ArithmeticInstruction::Sub => {
-                vec!["@SP", "A=A-1", "D=M", "A=A-1", "M=M-D", "@SP", "M=M-1"]
-                    .iter()
-                    .map(|s| s.to_string())
-                    .collect()
-            }
-            ArithmeticInstruction::Neg => vec!["@SP", "A=A-1", "M=-M"]
+            ArithmeticInstruction::Sub => vec![
+                "@SP", "A=M-1", "D=M", "@SP", "A=M-1", "A=A-1", "M=M-D", "@SP", "M=M-1",
+            ]
+            .iter()
+            .map(|s| s.to_string())
+            .collect(),
+            ArithmeticInstruction::Neg => vec!["@SP", "A=M-1", "M=-M"]
                 .iter()
                 .map(|s| s.to_string())
                 .collect(),
-            ArithmeticInstruction::Eq => vec![
-                "@SP",
-                "A=A-1",
-                "D=M",
-                "A=A-1",
-                "D=M-D",
-                "@EQ_TRUE",
-                "D;JEQ",
-                "@SP",
-                "A=A-2",
-                "M=0",
-                "@EQ_END",
-                "0;JMP",
-                "(EQ_TRUE)",
-                "@SP",
-                "A=A-2",
-                "M=1",
-                "(EQ_END)",
-                "@SP",
-                "M=M-1",
-            ]
-            .iter()
-            .map(|s| s.to_string())
-            .collect(),
-            ArithmeticInstruction::Gt => vec![
-                "@SP",
-                "A=A-1",
-                "D=M",
-                "A=A-1",
-                "D=M-D",
-                "@GT_TRUE",
-                "D;JGT",
-                "@SP",
-                "A=A-2",
-                "M=0",
-                "@GT_END",
-                "0;JMP",
-                "(GT_TRUE)",
-                "@SP",
-                "A=A-2",
-                "M=1",
-                "(GT_END)",
-                "@SP",
-                "M=M-1",
-            ]
-            .iter()
-            .map(|s| s.to_string())
-            .collect(),
-            ArithmeticInstruction::Lt => vec![
-                "@SP",
-                "A=A-1",
-                "D=M",
-                "A=A-1",
-                "D=M-D",
-                "@LT_TRUE",
-                "D;JLT",
-                "@SP",
-                "@SP",
-                "A=A-2",
-                "@LT_END",
-                "0;JMP",
-                "(LT_TRUE)",
-                "@SP",
-                "A=A-2",
-                "M=1",
-                "(LT_END)",
-                "@SP",
-                "M=M-1",
-            ]
-            .iter()
-            .map(|s| s.to_string())
-            .collect(),
+            ArithmeticInstruction::Eq => {
+                let label = Uuid::new_v4().to_string();
+                vec![
+                    "@SP",
+                    "A=M-1",
+                    "D=M",
+                    "A=A-1",
+                    "D=M-D",
+                    format!("@EQ_TRUE_{}", label).as_str(),
+                    "D;JEQ",
+                    "@SP",
+                    "A=M-1",
+                    "A=A-1",
+                    "M=0",
+                    format!("@EQ_END_{}", label).as_str(),
+                    "0;JMP",
+                    format!("(EQ_TRUE_{})", label).as_str(),
+                    "@SP",
+                    "A=M-1",
+                    "A=A-1",
+                    "M=-1",
+                    format!("(EQ_END_{})", label).as_str(),
+                    "@SP",
+                    "M=M-1",
+                ]
+                .iter()
+                .map(|s| s.to_string())
+                .collect()
+            }
+            ArithmeticInstruction::Gt => {
+                let label = Uuid::new_v4().to_string();
+                vec![
+                    "@SP",
+                    "A=M-1",
+                    "D=M",
+                    "A=A-1",
+                    "D=M-D",
+                    format!("@GT_TRUE_{}", label).as_str(),
+                    "D;JGT",
+                    "@SP",
+                    "A=M-1",
+                    "A=A-1",
+                    "M=0",
+                    format!("@GT_END_{}", label).as_str(),
+                    "0;JMP",
+                    format!("(GT_TRUE_{})", label).as_str(),
+                    "@SP",
+                    "A=M-1",
+                    "A=A-1",
+                    "M=-1",
+                    format!("(GT_END_{})", label).as_str(),
+                    "@SP",
+                    "M=M-1",
+                ]
+                .iter()
+                .map(|s| s.to_string())
+                .collect()
+            }
+            ArithmeticInstruction::Lt => {
+                let label = Uuid::new_v4().to_string();
+                vec![
+                    "@SP",
+                    "A=M-1",
+                    "D=M",
+                    "A=A-1",
+                    "D=M-D",
+                    format!("@LT_TRUE_{}", label).as_str(),
+                    "D;JLT",
+                    "@SP",
+                    "A=M-1",
+                    "A=A-1",
+                    "M=0",
+                    format!("@LT_END_{}", label).as_str(),
+                    "0;JMP",
+                    format!("(LT_TRUE_{})", label).as_str(),
+                    "@SP",
+                    "A=M-1",
+                    "A=A-1",
+                    "M=-1",
+                    format!("(LT_END_{})", label).as_str(),
+                    "@SP",
+                    "M=M-1",
+                ]
+                .iter()
+                .map(|s| s.to_string())
+                .collect()
+            }
             ArithmeticInstruction::And => {
-                vec!["@SP", "A=A-1", "D=M", "A=A-1", "M=D&M", "@SP", "M=M-1"]
+                vec!["@SP", "A=M-1", "D=M", "A=A-1", "M=D&M", "@SP", "M=M-1"]
                     .iter()
                     .map(|s| s.to_string())
                     .collect()
             }
             ArithmeticInstruction::Or => {
-                vec!["@SP", "A=A-1", "D=M", "A=A-1", "M=D|M", "@SP", "M=M-1"]
+                vec!["@SP", "A=M-1", "D=M", "A=A-1", "M=D|M", "@SP", "M=M-1"]
                     .iter()
                     .map(|s| s.to_string())
                     .collect()
             }
-            ArithmeticInstruction::Not => vec!["@SP", "A=A-1", "M=!M"]
+            ArithmeticInstruction::Not => vec!["@SP", "A=M-1", "M=!M"]
                 .iter()
                 .map(|s| s.to_string())
                 .collect(),
